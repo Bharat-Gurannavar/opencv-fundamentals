d62e1626f0ec369811d9fbb9253504d300e30d3d
Bharat-Gurannavar <gb6677370@gmail.com>
2026-07-10 13:40:28 +0530

    Create face_eye_detection.py

---
+++ 02-face-eye-detection/face_eye_detection.py
@@ -0,0 +1,36 @@
+import cv2
+
+frameWidth = 640
+frameHeight = 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(imgGray, 1.05, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi_gray = imgGray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eyeCascade.detectMultiScale(roi_gray, 1.05, 4)
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
+
+    cv2.imshow("Video", img)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
